50196196edbe4fcd6d7e8e5ca6698bc2c0697287
root <root@ubuntu-12.edu-wsb.local>
2024-06-23 04:42:59 +0200

    Added azure-functions folder with Terraform configuration

---
+++ azure-functions/main.tf
@@ -0,0 +1,47 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "service_plan" {
+  name                = var.service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_linux_function_app" "function_app" {
+  name                = var.function_app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.service_plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+
+  site_config {
+    application_stack {
+      python_version = "3.8"
+    }
+  }
+}
+
+resource "azurerm_function_app_function" "function" {
+  name             = var.function_name
+  resource_group_name = azurerm_resource_group.rg.name
+  function_app_id  = azurerm_linux_function_app.function_app.id
+  language         = "python"
+  file_path        = "path/to/your/function/code"
+}
+
---
+++ azure-functions/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  description = "Location of the resources"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+}
+
+variable "service_plan_name" {
+  description = "Name of the service plan"
+}
+
+variable "function_app_name" {
+  description = "Name of the function app"
+}
+
+variable "function_name" {
+  description = "Name of the function"
+}
+
